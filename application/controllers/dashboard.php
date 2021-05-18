a16e8959382a518ef65e9466fc9461b439a7f87d
ijlalbm <muhammadijlalm@gmail.com>
2021-05-18 08:22:22 +0800

    Konfigurasi model barang untuk menampilkan data

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -3,6 +3,7 @@
 class Dashboard extends CI_Controller {
 
     public function index(){
+        $data['barang'] = $this->model_barang->tampil_data()->result();
         $this->load->view('templates/header');
         $this->load->view('templates/sidebar');
         $this->load->view('dashboard');
