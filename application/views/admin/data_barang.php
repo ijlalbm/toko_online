5d3b6cc9e425d57d74a8740597961429dd0d2348
ijlalbm <muhammadijlalm@gmail.com>
2021-05-28 16:42:17 +0800

    add modal input barang

--- application/views/admin/data_barang.php
+++ application/views/admin/data_barang.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-    <button class="btn btn-sm btn-primary mb-2"><i class="fas fa-plus fa sm"></i>Tambah Barang</button>
+    <button class="btn btn-sm btn-primary mb-2" data-target="#data_barang" data-toggle="modal"><i class="fas fa-plus fa sm"></i>Tambah Barang</button>
     <table class="table table-bordered">
         <tr>
             <th>NO</th>
@@ -29,4 +29,25 @@
                 <?php endforeach; ?>
         
     </table>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="data_barang" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate aspernatur iste omnis obcaecati! Enim non, deserunt ipsa beatae a velit labore quia, asperiores suscipit maxime, ea optio officiis dignissimos amet!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
